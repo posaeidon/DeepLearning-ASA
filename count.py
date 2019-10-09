9e9c64a389eb011b655ffca71f051873ee1c59c2
Saeid Mahmoudzadeh <s.mahmoudzadeh@pczaal4.strw.leidenuniv.nl>
2019-10-09 15:53:43 +0200

    update

--- count.py
+++ count.py
@@ -1,9 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-output = np.loadtxt("output_ho.txt")
+output = np.loadtxt("output_test.txt")
 
 hits = 0
 for elem in output[:, 2]:
     if elem <= 0.05:
         hits+=1
 print(hits, hits/len(output[:, 2]))
+
+plt.hist(output[:,2], bins = 50)
+plt.show()
